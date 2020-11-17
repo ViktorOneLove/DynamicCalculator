5e491b0c15b7a5dee86abbf5b55cba327eee5290
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-11-18 02:48:05 +0300

    Create openBracketDLL.cpp

---
+++ dllOperations/openBracketDLL.cpp
@@ -0,0 +1,25 @@
+#include "BaseMath.h"
+
+
+
+class OpenBracket : public IBaseMath {
+
+	~OpenBracket() {};
+
+	virtual std::pair<OPERATION_ERROR, double> DoFunc(std::vector<double> params) override { 
+		return std::make_pair(OPERATION_ERROR::SUCCESS, NONE);
+	}
+
+	virtual int GetOperationPrioraty() override { return OPERATION_TYPE::BRACKET; }
+
+	virtual int GetNumOfArguments() override { return 0; }
+
+	virtual void Destroy() override { delete this; }
+
+	virtual std::string GetFuncToken() override { return "("; }
+};
+
+extern "C" __declspec(dllexport) IBaseMath * __cdecl createClass()
+{
+	return new OpenBracket;
+}
