6f508351b567692de377c7569d267b09e946a870
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-11-18 02:50:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,23 +11,5 @@ int main() {
 
 	calc.Process("plugins");
 
-	//calc.Process();
-
-	/*calc.LoadAllDLL();
-
-	HINSTANCE hdll = NULL;
-
-	hdll = ::LoadLibrary(L"D\\Programming\\C_or_C++\\Calculator\\Calculator\\plugins\\sinDLL.dll");
-
-	baseMathFactory factoryFunc = (baseMathFactory)(
-		::GetProcAddress(hdll, "createClass"));
-
-	IBaseMath* instance = factoryFunc();
-
-	instance->Destroy();
-
-	FreeLibrary(hdll);*/
-
-
 	return 0;
 }
